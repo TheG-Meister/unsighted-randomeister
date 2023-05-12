6eb633574c5defd273c0a5fa628ac67e2ab6c5c3
The G-Meister <thegiemeister@gmail.com>
2023-05-12 18:34:26 +0100

    Add MovementLogger

---
+++ Code/logging/MovementLogger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace dev.gmeister.unsighted.randomeister.logging;
+
+public class MovementLogger
+{
+    private string? currentLocation;
+
+	public MovementLogger()
+	{
+
+	}
+
+	private string BoolToString(bool value)
+	{
+		return value ? "1" : "";
+	}
+
+	public void SetLocation(string location, bool transition, bool teleport)
+	{
+		if (currentLocation != null && currentLocation != location)
+        {
+			List<string> fields = new() { currentLocation, location, BoolToString(transition), BoolToString(teleport) };
+			Plugin.Instance.GetLogger().LogInfo(string.Join("\t", fields));
+        }
+        currentLocation = location;
+    }
+
+}
