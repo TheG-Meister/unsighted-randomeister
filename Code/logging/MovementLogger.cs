3664eadfff119d12c49e7dc40878a2312e6f95b3
The G-Meister <thegiemeister@gmail.com>
2023-05-12 19:01:25 +0100

    Log items and data in movementlogger

--- Code/logging/MovementLogger.cs
+++ Code/logging/MovementLogger.cs
@@ -20,7 +20,20 @@ public class MovementLogger
 	{
 		if (currentLocation != null && currentLocation != location)
         {
-			List<string> fields = new() { currentLocation, location, BoolToString(transition), BoolToString(teleport) };
+			List<string> printItemNames = new();
+
+			List<string> weaponNames = PseudoSingleton<Helpers>.instance.GetPlayerData().currentWeapons.Select(data => data.equipmentName).ToList();
+			printItemNames.AddRange(weaponNames);
+
+			List<string> itemNames = PseudoSingleton<Helpers>.instance.GetPlayerData().playerItems.Select(data => data.itemName).ToList();
+			if (itemNames.Contains("JumpBoots")) printItemNames.Add("JumpBoots");
+            if (itemNames.Contains("Spinner")) printItemNames.Add("Spinner");
+
+            string items = string.Join(",", printItemNames);
+			string data = string.Join(",", PseudoSingleton<Helpers>.instance.GetPlayerData().dataStrings);
+
+
+            List<string> fields = new() { currentLocation, location, BoolToString(transition), BoolToString(teleport), items, data};
 			Plugin.Instance.GetLogger().LogInfo(string.Join("\t", fields));
         }
         currentLocation = location;
