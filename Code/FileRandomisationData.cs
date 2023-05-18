034f8bf586cf1744b244718d9fee3a1eebc986b8
The G-Meister <thegiemeister@gmail.com>
2023-05-18 11:59:50 +0100

    Add FileRandomisationData

---
+++ Code/FileRandomisationData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dev.gmeister.unsighted.randomeister;
+
+/**
+ * Contains data on how a save slot was randomised, and how to restore that randomisation when the file is reloaded.
+**/
+[Serializable]
+internal class FileRandomisationData
+{
+    public bool randomise;
+    public bool randomSeed;
+    public int seed;
+    public bool randomiseChests;
+
+    public FileRandomisationData(bool randomised, bool randomisedChests, bool randomisedSeed, int seed)
+    {
+        this.randomise = randomised;
+        this.randomiseChests = randomisedChests;
+        this.randomSeed = randomisedSeed;
+        this.seed = seed;
+    }
+}
