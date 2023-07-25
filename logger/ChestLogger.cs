982e6cfea70a70fa93f50924d39aaff453961e01
The G-Meister <thegiemeister@gmail.com>
2023-07-25 13:52:05 +0100

    Change chest naming

--- logger/ChestLogger.cs
+++ logger/ChestLogger.cs
@@ -38,7 +38,7 @@ public class ChestLogger
 
         string path = Path.Combine(dir, file);
 
-        string line = scene + "-" + chest;
+        string line = $"{scene}_{chest}";
         File.AppendAllLines(path, new List<string>() { line });
     }
 
