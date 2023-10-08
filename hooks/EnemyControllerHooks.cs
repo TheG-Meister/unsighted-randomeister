e017cca4ae08bceae94bb6178f9cb02a93b8dfe9
The G-Meister <thegiemeister@gmail.com>
2023-10-08 11:56:16 +0100

    Make enemy drops refer to plugin drop tables

--- hooks/EnemyControllerHooks.cs
+++ hooks/EnemyControllerHooks.cs
@@ -20,14 +20,10 @@ public class EnemyControllerHooks
         if (Plugin.Instance != null && Plugin.Instance.currentData != null)
         {
             Dictionary<string, DropController> dropTables = Plugin.Instance.currentData.enemyDropTables;
-            if (!dropTables.ContainsKey(__instance.GetType().Name))
+            if (dropTables.ContainsKey(__instance.GetType().Name))
             {
-                List<string> materials = PseudoSingleton<Lists>.instance.itemDatabase.itemList.Where(item => item.itemType == ItemType.material).Select(item => item.itemName).ToList();
-
-                List<string> controllerList = new List<string>() { "", materials[new System.Random().Next(materials.Count)] };
-                dropTables.Add(__instance.GetType().Name, new DropController() { enemyNameID = __instance.GetType().Name, itemList = controllerList } );
+                __instance.dropController = dropTables[__instance.GetType().Name];
             }
-            __instance.dropController = dropTables[__instance.GetType().Name];
         }
     }
 
