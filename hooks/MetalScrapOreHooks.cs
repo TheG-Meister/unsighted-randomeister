00893527a128c0f1ebb7e47fd3672ccd520b4586
The G-Meister <thegiemeister@gmail.com>
2023-10-13 20:39:57 +0100

    Change material crystal drops

---
+++ hooks/MetalScrapOreHooks.cs
@@ -0,0 +1,52 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dev.gmeister.unsighted.randomeister.hooks;
+
+[Harmony]
+public class MetalScrapOreHooks
+{
+
+    [HarmonyPatch(typeof(MetalScrapOre), nameof(MetalScrapOre.Destroyed)), HarmonyTranspiler]
+    public static IEnumerable<CodeInstruction> MetalScrapOreDestroyedTranspiler(IEnumerable<CodeInstruction> instructions)
+    {
+
+        List<CodeInstruction> result = new(instructions);
+        int firstStloc_1 = -1;
+
+        for (int i = 0; i < result.Count; i++)
+        {
+            if (result[i].opcode == OpCodes.Stloc_1)
+            {
+                firstStloc_1 = i;
+                break;
+            }
+        }
+
+        if (firstStloc_1 > -1)
+        {
+            List<CodeInstruction> addedCodes = new(instructions)
+            {
+                new CodeInstruction(OpCodes.Ldarg_0),
+                new CodeInstruction(OpCodes.Ldloc_1),
+                new CodeInstruction(OpCodes.Call, AccessTools.DeclaredMethod(typeof(MetalScrapOreHooks), nameof(MetalScrapOreHooks.ReplaceMaterialCrystalItem), new Type[] { typeof(MetalScrapOre), typeof(string) })),
+                new CodeInstruction(OpCodes.Stloc_1),
+            };
+
+            result.InsertRange(firstStloc_1 + 1, addedCodes);
+        }
+
+        return result;
+    }
+
+    public static string ReplaceMaterialCrystalItem(MetalScrapOre crystal, string item)
+    {
+        return "Null";
+    }
+
+}
