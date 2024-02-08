8f4c14057615d726d45413e9773a5dfd3aab3329
The G-Meister <thegiemeister@gmail.com>
2024-02-08 23:12:55 +0000

    Remove duplicate changing scene set

--- logger/MovementLogger.cs
+++ logger/MovementLogger.cs
@@ -329,7 +329,6 @@ public class MovementLogger : IDisposable
 
         this.gameTime = gameTime;
         this.realTime = realTime;
-        this.changingScene = changingScene;
 
         if (!intermediate)
         {
