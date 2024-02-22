e61aa23a0aed299ff1d26b23ce39551e75adc8b0
The G-Meister <thegiemeister@gmail.com>
2024-02-22 15:38:08 +0000

    Add delimited file reader

---
+++ core/DelimitedFileReader.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dev.gmeister.unsighted.randomeister.core;
+
+public class DelimitedFileReader
+{
+
+    public static IEnumerator<List<string>> ReadDelimitedFile(string path, char delim, string[] headers)
+    {
+        if (File.Exists(path))
+        {
+            List<string> lines = new(File.ReadAllLines(path));
+
+            List<string> fileHeaders = null;
+            foreach (string line in lines)
+            {
+                if (!string.IsNullOrEmpty(line) && !line.StartsWith("#"))
+                {
+                    string[] split = line.Split(delim);
+
+                    if (fileHeaders == null)
+                    {
+                        foreach (string header in headers) if (!split.Contains(header)) throw new IOException("A header was missing from the passed file");
+                        fileHeaders = new(split);
+                    }
+                    else
+                    {
+                        List<string> output = new();
+                        foreach (string header in headers)
+                        {
+                            if (split.Length <= fileHeaders.IndexOf(header)) throw new IOException("A line did not have enough fields");
+                            output.Add(split[fileHeaders.IndexOf(header)]);
+                        }
+                        yield return output;
+                    }
+                }
+            }
+        }
+    }
+
+}
