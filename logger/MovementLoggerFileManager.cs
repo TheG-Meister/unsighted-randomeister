c9b5f4ea47cfa4747a07bb17bcc775af43cd17e6
The G-Meister <thegiemeister@gmail.com>
2024-11-29 15:16:37 +0000

    update file manager

--- logger/MovementLoggerFileManager.cs
+++ logger/MovementLoggerFileManager.cs
@@ -12,19 +12,57 @@ public class MovementLoggerFileManager
 {
 
     public string path;
-    public string backupsPath;
-    public string completePath;
+    public string backupsDir;
+    public string completeDir;
+    public string brokenDir;
+    public string currentZip;
+    public MovementLoggerFileZip currentBatch;
+    public MovementLoggerFileZip currentCommandBatch;
 
     public MovementLoggerFileManager(string path)
     {
         this.path = path;
-        this.backupsPath = Path.Combine(this.path, "backups");
-        this.completePath = Path.Combine(this.path, "complete");
+        this.backupsDir = Path.Combine(this.path, "backups");
+        this.completeDir = Path.Combine(this.path, "complete");
+        this.brokenDir = Path.Combine(this.path, "broken");
+        this.currentZip = Path.Combine(this.path, "current.zip");
 
         Directory.CreateDirectory(this.path);
-        Directory.CreateDirectory(this.backupsPath);
-        Directory.CreateDirectory(this.completePath);
-        
+        Directory.CreateDirectory(this.backupsDir);
+        Directory.CreateDirectory(this.completeDir);
+        Directory.CreateDirectory(this.brokenDir);
+
+        if (!Path.Exists(this.currentZip))
+        {
+            this.CreateCurrentZip();
+            this.currentBatch = new(this.currentZip);
+            this.currentBatch.CreateAll();
+            this.currentBatch.ResetAll();
+        }
+        else
+        {
+            this.currentBatch = new(this.currentZip);
+            this.currentBatch.Open();
+        }
+    }
+
+    private void CreateCurrentZip()
+    {
+        string tempDir = Path.Combine(Path.GetDirectoryName(path), Path.GetFileNameWithoutExtension(path));
+        Random random = new();
+        if (Directory.Exists(tempDir))
+        {
+            tempDir += "-temp-";
+            do
+            {
+                tempDir += Constants.ALPHANUMERIC_CHARS[random.Next(Constants.ALPHANUMERIC_CHARS.Length)];
+            }
+            while (Directory.Exists(tempDir));
+        }
+
+        Directory.CreateDirectory(tempDir);
+        ZipFile.CreateFromDirectory(tempDir, path);
+        Directory.Delete(tempDir, true);
     }
 
     public void CreateZip(List<string> files, string path)
