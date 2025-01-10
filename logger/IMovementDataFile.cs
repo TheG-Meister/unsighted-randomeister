b318baf763d9d38d22ad6fc637ab0db2cb3349e9
The G-Meister <thegiemeister@gmail.com>
2025-01-10 21:03:52 +0000

    remove virtual tag on movementdatafile create method

--- logger/IMovementDataFile.cs
+++ logger/IMovementDataFile.cs
@@ -16,6 +16,6 @@ public interface IMovementDataFile : IDelimitedFile
 
     //public IReadOnlyDictionary<int, T> ParsedData { get; }
 
-    public virtual void Create();
+    public void Create();
 
 }
